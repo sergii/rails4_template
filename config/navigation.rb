c8911f1368d8673f54480400cf5487ee9cf46d99
Alexander Logunov <unlovedru@gmail.com>
2013-10-17 17:44:40 +0400

    правка стилей под новый бутстрап

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,9 +3,9 @@
 
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-    # primary.item :hotels, I18n.t("admin.menu.hotels"), hotels_url
+    #primary.item :hotels, I18n.t("admin.menu.hotels"), root_url
 
-    primary.dom_class = 'nav pull-right'
+    primary.dom_class = 'nav navbar-nav pull-right'
 
     # you can turn off auto highlighting for a specific level
     primary.auto_highlight = true
